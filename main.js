936e00de53f4339b46dff02252f5042778cd2e26
edoardocollo <edoardo.collo@gmail.com>
2020-12-10 15:49:15 +0100

    mileston2

--- main.js
+++ main.js
@@ -23,35 +23,48 @@ iconsArray.push(new icon('ninja', 'fas', 'fa-user-ninja','utenti'));
 iconsArray.push(new icon('impiegato', 'fas', 'fa-user-tie','utenti'));
 iconsArray.push(new icon('spia', 'fas', 'fa-user-secret','utenti'));
 iconsArray.push(new icon('dottore', 'fas', 'fa-user-md','utenti'));
-iconsArray.push(new icon('rider', 'fas', 'fa-user-injured','animali'));
-iconsArray.push(new icon('studente', 'fas', 'fa-user-graduate','animali'));
+iconsArray.push(new icon('rider', 'fas', 'fa-user-injured','utenti'));
+iconsArray.push(new icon('studente', 'fas', 'fa-user-graduate','utenti'));
 console.log(iconsArray);
   // Semezioniamo il container icons
 const mainTarget = $('main');
 
-  //inseriamo le icone nel container (possiamo creare una funzione tipo print() per inserire gli elementi e richiamarla qui)
+ //aggiungiamo dei colori usando una funzione
+  iconsArray.forEach(colorForFamily);
 
-iconsArray.forEach(printElement);
+ //inseriamo le icone nel container (possiamo creare una funzione tipo print() per inserire gli elementi e richiamarla qui)
 
-
-
-
-
+ iconsArray.forEach(printElement);
 
 /* ---- FUNCTIONS ----*/
 
 
 function printElement(element){
   let name = element.name;
+  let colore = element.colore;
   let prefix = element.prefix;
   let type = element.type;
   let family = element.family;
   mainTarget.append(`
     <div class="card ${family}">
-    <i class="${prefix} ${type}"></i>
+    <i class="${prefix} ${type}"style="color:${colore};"></i>
     <h3>${name}</h3>
     </div>
     `);
   }
 
+  function colorForFamily (element){
+    // definiamo dei colori per le icone (blue, orange, purple)
+    var blu = 'blue';
+    var arancione = 'orange';
+    var viola = 'purple';
+    if (element.family == 'animali') {
+      element.colore = blu;
+    }else if (element.family == 'cibo') {
+      element.colore = arancione;
+    }else if (element.family == 'utenti') {
+      element.colore = viola;
+    }
+
+  }
 });
