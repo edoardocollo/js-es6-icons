d677264e5a9f499ed9d6ec62df0045cef19cbbe3
edoardocollo <edoardo.collo@gmail.com>
2020-12-11 11:05:22 +0100

    some review

--- main.js
+++ main.js
@@ -1,40 +1,17 @@
 $(function() {
   // Creiamo array di oggetti per le icone con le seguenti proprieta: name, prefix, type, family
-class icon {
-  constructor(name, prefix, type, family) {
-    this.name = name;
-    this.prefix = prefix;
-    this.type = type;
-    this.family = family
-  }
-}
-const iconsArray = [];
-iconsArray.push(new icon('cane', 'fas', 'fa-dog','animali'));
-iconsArray.push(new icon('gatto', 'fas', 'fa-cat','animali'));
-iconsArray.push(new icon('colomba', 'fas', 'fa-dove','animali'));
-iconsArray.push(new icon('cavallo', 'fas', 'fa-horse','animali'));
-iconsArray.push(new icon('pesce', 'fas', 'fa-fish','animali'));
-iconsArray.push(new icon('ippopotamo', 'fas', 'fa-hippo','animali'));
-iconsArray.push(new icon('mela', 'fas', 'fa-apple-alt','cibo'));
-iconsArray.push(new icon('pizza', 'fas', 'fa-pizza-slice','cibo'));
-iconsArray.push(new icon('hamburger', 'fas', 'fa-hamburger','cibo'));
-iconsArray.push(new icon('hotdog', 'fas', 'fa-hotdog','cibo'));
-iconsArray.push(new icon('ninja', 'fas', 'fa-user-ninja','utenti'));
-iconsArray.push(new icon('impiegato', 'fas', 'fa-user-tie','utenti'));
-iconsArray.push(new icon('spia', 'fas', 'fa-user-secret','utenti'));
-iconsArray.push(new icon('dottore', 'fas', 'fa-user-md','utenti'));
-iconsArray.push(new icon('rider', 'fas', 'fa-user-injured','utenti'));
-iconsArray.push(new icon('studente', 'fas', 'fa-user-graduate','utenti'));
-// console.log(iconsArray);
-  // Semezioniamo il container icons
-const mainTarget = $('main');
-
+const iconsArray = objectCreator();
+console.log(iconsArray);
+const categorie = arrayCategories(iconsArray);
+console.log(categorie);
  //aggiungiamo dei colori usando una funzione
-  iconsArray.forEach(colorForFamily);
+iconsArray.forEach(colorForFamily);
 
+// Semezioniamo il container icons
+const mainTarget = $('main');
  //inseriamo le icone nel container (possiamo creare una funzione tipo print() per inserire gli elementi e richiamarla qui)
 
- iconsArray.forEach(printElement);
+iconsArray.forEach(printElement);
 
 
  const tutti = $('#tutti');
@@ -75,9 +52,48 @@ const mainTarget = $('main');
 
 
 /* ---- FUNCTIONS ----*/
+function objectCreator(){
+  class icon {
+    constructor(name, prefix, type, family) {
+      this.name = name;
+      this.prefix = prefix;
+      this.type = type;
+      this.family = family
+    }
+  }
 
+  const contenitore = [];
+  contenitore.push(new icon('cane', 'fas', 'fa-dog','animali'));
+  contenitore.push(new icon('gatto', 'fas', 'fa-cat','animali'));
+  contenitore.push(new icon('colomba', 'fas', 'fa-dove','animali'));
+  contenitore.push(new icon('cavallo', 'fas', 'fa-horse','animali'));
+  contenitore.push(new icon('pesce', 'fas', 'fa-fish','animali'));
+  contenitore.push(new icon('ippopotamo', 'fas', 'fa-hippo','animali'));
+  contenitore.push(new icon('mela', 'fas', 'fa-apple-alt','cibo'));
+  contenitore.push(new icon('pizza', 'fas', 'fa-pizza-slice','cibo'));
+  contenitore.push(new icon('hamburger', 'fas', 'fa-hamburger','cibo'));
+  contenitore.push(new icon('hotdog', 'fas', 'fa-hotdog','cibo'));
+  contenitore.push(new icon('ninja', 'fas', 'fa-user-ninja','utenti'));
+  contenitore.push(new icon('impiegato', 'fas', 'fa-user-tie','utenti'));
+  contenitore.push(new icon('spia', 'fas', 'fa-user-secret','utenti'));
+  contenitore.push(new icon('dottore', 'fas', 'fa-user-md','utenti'));
+  contenitore.push(new icon('rider', 'fas', 'fa-user-injured','utenti'));
+  contenitore.push(new icon('studente', 'fas', 'fa-user-graduate','utenti'));
+  return contenitore;
+}
+
+function arrayCategories(arrayTarget){
+  const categories = [];
+  arrayTarget.forEach(function(element){
+    if (!categories.includes(element.family)) {
+      categories.push(element.family);
+    }
+  });
+  return categories;
+}
 
 function printElement(element){
+  // utilizzare destructuring
   const name = element.name;
   const colore = element.colore;
   const prefix = element.prefix;
